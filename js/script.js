71941a5e22830f77e9724ddff044262aa901a6b6
martingarrote <martindambrowskigarrote@gmail.com>
2025-01-04 12:47:31 -0300

    add project section and modal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-const header = document.querySelector('header');
+const header = document.querySelector("header");
+const modal = document.querySelector(".modal");
+const closeModal = document.getElementById("close-modal");
+
+function onClick(event) {
+  if (event.target === modal) {
+    modal.close();
+  }
+}
+
+modal.addEventListener("click", onClick);
+modal.showModal();
+
+// closeModal.onclick = modal.close();
 
 document.addEventListener("DOMContentLoaded", () => {
     loadTechs();
     loadAbout();
-    // loadProjects();
+    loadProjects();
+    closeModal.onclick = modal.close();
 })
 
 window.addEventListener('scroll', () => {
@@ -118,4 +132,89 @@ function copyAboutSummary() {
   const summary = localStorage.getItem("summary")
 
   navigator.clipboard.writeText(summary);
+}
+
+function loadProjects() {
+  fetch("../data/projects.json")
+    .then(response => response.json())
+    .then(projects => {
+      insertProjects(projects)
+
+      const openIFrameNodes = document.querySelectorAll(".open-iframe");
+      const imageNodes = document.querySelectorAll(".project-image")
+
+      openIFrameNodes.forEach(node => {
+        node.addEventListener("click", () => {
+          const target = event.target.parentNode;
+
+          insertIFrame(target.getAttribute("data-url"));
+          modal.showModal();
+        })
+      });
+
+      imageNodes.forEach(image => {
+        image.addEventListener("click", () => {
+          const target = event.target;
+
+          insertImage(target.src)
+          modal.showModal();
+        })
+      })
+
+    })
+    .catch(error => console.log(error));
+}
+
+function insertProjects(projects) {
+  const projectContainer = document.getElementById("project-container");
+
+  projectContainer.innerHTML = ""
+
+  projects.forEach(p => {
+    projectContainer.innerHTML += (createProjectStructure(p));
+  });
+
+}
+
+function createProjectStructure(project) {
+  let tags = "";
+  let buttons = "";
+  let description = "";
+
+  project.tags.forEach(tag => {
+    tags += `<span class="tag">${tag}</span>`
+  });
+
+  buttons += project.website ? `<a href="${project.website}" class="project-btn"><i class="ph ph-globe"></i></a>` : "";
+  buttons += project.github ? `<a href="${project.github}" class="project-btn"><i class="ph ph-github-logo"></i></a>` : "";
+  buttons += project.openable ? `<a class="project-btn open-iframe" data-url="${project.website}"><i class="ph ph-frame-corners"></i></a>` : "";
+
+  description += project.description.part1 ? `<p>${project.description.part1}</p>` : "";
+  description += project.description.part2 ? `<p>${project.description.part2}</p>` : "";
+  description += project.description.part3 ? `<p>${project.description.part3}</p>` : "";
+
+  return `
+  <div class="project">
+    <div class="img-container">
+      <img class="project-image" src="${project.image}" alt="${project.title}">
+    </div>
+    <div class="project-content">
+        <h3>${project.title}</h3>
+        ${description}
+        <div class="tags">${tags}</div>
+    </div>
+    <span class="line"></span>
+    <div class="project-buttons">
+        ${buttons}
+    </div>
+  </div>
+  `;
+}
+
+function insertIFrame(url) {
+  modal.innerHTML = `<iframe src="${url}"></iframe>`
+}
+
+function insertImage(source) {
+  modal.innerHTML = `<img src="${source}">`
 }
